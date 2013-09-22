LineageOS/android_packages_apps_Trebuchet

Trebuchet : Support for custom icons per shortcut
Allows user to assign custom icons from icon packs to individual
homescreen shortcuts.

Change-Id: I6613b5f3e6517485a6f83aa5f6769e22bb7f18dc

src/com/cyanogenmod/trebuchet/BubbleTextView.java
@@ -170,6 +170,13 @@ public class BubbleTextView extends TextView implements ShortcutInfo.ShortcutLis
         }
     }
 
+    @Override
+    public void onIconChanged(Bitmap b) {
+        setCompoundDrawablesWithIntrinsicBounds(null,
+                new FastBitmapDrawable(b),
+                null, null);
+    }
+
     /**
      * Draw this BubbleTextView into the given Canvas.
      *

ReVolt-ROM/packages_apps_Trebuchet

src/com/cyanogenmod/trebuchet/BubbleTextView.java
@@ -162,12 +162,19 @@ public class BubbleTextView extends TextView implements ShortcutInfo.ShortcutLis
     }
 
     @Override
-    public void onTitleChanged(CharSequence title) {
+    public void onTitleChanged(ShortcutInfo item) {
         if (mTextVisible) {
-            setText(title);
+            setText(item.title);
         } else {
-            mVisibleText = title;
+            mVisibleText = item.title;
         }
+    }
+
+    @Override
+    public void onIconChanged(ShortcutInfo item) {
+        setCompoundDrawablesWithIntrinsicBounds(null,
+                new FastBitmapDrawable(item.getIcon(null)),
+                null, null);
     }
 
     /**